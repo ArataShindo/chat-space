1073756c2de7a0005df2c270e77b077312a81ab8
aratashindo <k.kondo.newark@gmail.com>
2020-02-17 21:34:17 +0900

    非同期２

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,52 +1,45 @@
 $(function(){
   function buildHTML(message){
-
-
-      var addImage = (message.image !== null) ? `<img class = "lower-message__image", src="${message.image}">` :''
-      var html = `<div class="message" data-message-id=${message.id}>
-                  <div class="upper-message">
-                  <div class="upper-message__user-name">
-                  ${message.name}
-                  </div>
-                  <div class="upper-message__date">
-                  ${message.updated_at}
-                  </div>
-                  </div>
-                  <div class="lower-message">
-                  <p class="lower-message__content">
-                  ${message.content}
-                  </p>
-                  </div>
-                  ${addImage}
-                  </div>`
-                              
-                return html
+    var addImage = (message.image !== null) ? `<img class = "lower-message__image", src="${message.image}">` :''
+    var html = `<div class="message" data-message-id=${message.id}>
+      <div class="upper-message">
+        <div class="upper-message__user-name">
+          ${message.name}
+        </div>
+        <div class="upper-message__date">
+          {message.updated_at}
+        </div>
+      </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+        ${addImage}
+      </div>`
+      return html
   }
-
-
-
-
-    $('.new_message').on('submit', function(e) {
-      e.preventDefault();
+  $('.new_message').on('submit', function(e) {
+    e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action'); 
   
-      $.ajax({
-        url: url, 
-        type: 'POST',  
-        data: formData,  
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      })
-      .done(function(message){
-        var html = buildHTML(message);
-        $('.messages').append(html);
-        $('form')[0].reset();
-        $(".form__submit").removeAttr("disabled");
-        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+  $.ajax({
+    url: url, 
+    type: 'POST',  
+    data: formData,  
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+    .done(function(message){
+      var html = buildHTML(message);
+      $('.messages').append(html);
+      $('form')[0].reset();
+      $(".form__submit").removeAttr("disabled");
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
 
-      })
+    })
       .fail(function(massege){
         alert('メッセージを入力してください');
         $(".form__submit").removeAttr("disabled");
@@ -66,13 +59,8 @@ $(function(){
         data: {id : last_message_id },
         dataType: 'json'
       })
-
-
-      .done(function(messages){
-  
-          
-        
-        var insertHTML = '';
+　　　　.done(function(messages){
+　　　　　var insertHTML = '';
         messages.forEach(function (message) {
           insertHTML = buildHTML(message); 
           $('.messages').append(insertHTML);
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,5 @@
-json.user_name @message.user.name
-json.created_at @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
 json.content @message.content
-json.image @message.image_url
+json.name @message.user.name
+json.updated_at @message.updated_at.strftime("%Y/%m/%d %H:%M")
+json.image @message.image.url
+json.id @message.id
