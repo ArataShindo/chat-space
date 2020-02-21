2fcd7aad572814918ee0d900db3bb1cef932ebd6
aratashindo <k.kondo.newark@gmail.com>
2020-02-21 22:11:16 +0900

    修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'ziko7110image'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ziko7110image'
+  config.fog_directory  = 'chat-space2020202020'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space2020202020'
 end
