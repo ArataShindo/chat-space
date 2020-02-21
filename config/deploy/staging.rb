104ba7d8ca5a2d9ab00a3f676050009c7a14982c
aratashindo <k.kondo.newark@gmail.com>
2020-02-21 19:56:15 +0900

    @@@2

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,3 @@
-server '3.20.242.212', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
